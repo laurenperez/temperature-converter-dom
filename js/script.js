8e1492bb62f45d9aca33a3a1b99364ec17ea4492
laurenperez <laurenashleymarks@gmail.com>
2018-01-26 10:50:49 -0800

    refactored a bit for student demo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Hello, front end');
+
+var radioF = document.getElementById('F');
+var radioC = document.getElementById('C');
+var outputTemp = document.getElementById("convertedTemp");
+var data = document.getElementById('startTemp')
+var convertedF;
+var convertedC;
+var data;
+
+document.getElementById('submit').onclick = function(){
+  console.log(data.value);
+  switch(true){
+    case radioF.checked === true:
+    convertedF = Math.floor((data.value-32)*5/9) + "° C";
+    outputTemp.textContent = convertedF;
+    break;
+    case radioC.checked === true:
+    convertedC = Math.floor((data.value*9/5)+32) + "° F";
+    outputTemp.textContent = convertedC;
+    break;
+  };
+};
+
+document.getElementById('clear').onclick = function(){
+  outputTemp.textContent = '';
+  data.value = '';
+  radioF.checked = false;
+  radioC.checked = false;
+}
